e016297046fd5c468ff9a780b17257e4c03120f5
taylrun <14286600+taylrun@users.noreply.github.com>
2023-07-04 18:48:03 -0700

    graceful exit 1

--- cli.js
+++ cli.js
@@ -4,6 +4,10 @@ import child_process from 'child_process'
 
 const tapeArgs = process.argv.slice(2)
 
-child_process.execSync(`tape ${tapeArgs.join(' ')} | tap-pretty`, {
-  stdio: ['pipe', process.stdout, process.stderr],
-})
+try {
+  child_process.execSync(`tape ${tapeArgs.join(' ')} | tap-pretty`, {
+    stdio: ['pipe', process.stdout, process.stderr],
+  })
+} catch (err) {
+  process.exit(1)
+}
---
+++ tests/fail.test.js
@@ -0,0 +1,5 @@
+import test from '../boxtape.js'
+
+test('fail', (t) => {
+  t.equal(1, 2)
+})
