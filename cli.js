6eceb28b55c727c171e686d2089a1560d5893719
taylrun <14286600+taylrun@users.noreply.github.com>
2023-07-06 17:16:44 -0700

    remove logs

--- cli.js
+++ cli.js
@@ -6,13 +6,9 @@ import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-console.log('__dirname', __dirname)
-console.log('process.cwd()', process.cwd())
-
 const tapeArgs = process.argv.slice(2)
 const binPath = path.resolve(__dirname, 'node_modules', '.bin')
 const newPath = binPath + ":" + process.env.PATH
-console.log('newPath', newPath)
 
 try {
   child_process.execSync(`tape ${tapeArgs.join(' ')} | tap-pretty`, {
